cfd5aa1fe4a2b07cd35981ef369232fad7837bf4
blessy-JG123 <blessyjg105@gmail.com>
2025-08-08 19:03:49 +0530

    Create StudentServiceImpl.java

---
+++ StudentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.student.management.system.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.student.management.system.entity.Student;
+import com.example.student.management.system.repository.StudentRepository;
+import com.example.student.management.system.service.StudentService;
+
+
+@Service
+	public class StudentServiceImpl implements StudentService{
+
+		private StudentRepository studentRepository;
+		
+		public StudentServiceImpl(StudentRepository studentRepository) {
+			super();
+			this.studentRepository = studentRepository;
+		}
+@Override
+		public List<Student> getAllStudents() {
+			return studentRepository.findAll();
+		}
+
+		@Override
+		public Student saveStudent(Student student) {
+			return studentRepository.save(student);
+		}
+
+		@Override
+		public Student getStudentById(Long id) {
+			return studentRepository.findById(id).get();
+		}
+
+		@Override
+		public Student updateStudent(Student student) {
+			return studentRepository.save(student);
+		}
+
+		@Override
+		public void deleteStudentById(Long id) {
+			studentRepository.deleteById(id);	
+		}
+
+	}
+
