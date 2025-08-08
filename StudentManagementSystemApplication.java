1dc8fb2c9ab051f8d44eba80be8b23ca8aff2dd9
blessy-JG123 <blessyjg105@gmail.com>
2025-08-08 19:02:02 +0530

    Create StudentManagementSystemApplication.java

---
+++ StudentManagementSystemApplication.java
@@ -0,0 +1,27 @@
+package com.example.student.management.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.student.management.system.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		
+	}
+
+}
