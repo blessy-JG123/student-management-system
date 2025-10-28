b17ca06bd33f6f20b639ce68eaf27080790f47ea
blessy-JG123 <blessyjg105@gmail.com>
2025-10-28 15:32:41 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,63 +1,58 @@
 package com.example.student.management.system.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-	@Table(name = "students")
-	public class Student {
-		
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		
-		@Column(name = "first_name", nullable = false)
-		private String firstName;
-		
-		@Column(name = "last_name")
-		private String lastName;
-		
-		@Column(name = "email")
-		private String email;
-		
-		public Student() {
-			
-		}
-		
-		public Student(String firstName, String lastName, String email) {
-			super();
-			this.firstName = firstName;
-			this.lastName = lastName;
-			this.email = email;
-		}
-		public Long getId() {
-			return id;
-		}
-		public void setId(Long id) {
-			this.id = id;
-		}
-		public String getFirstName() {
-			return firstName;
-		}
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
-		public String getLastName() {
-			return lastName;
-		}
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-	}
+@Table(name = "students")
+public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "semester")
+    private String semester;
+
+    @Column(name = "interested_domain")
+    private String interestedDomain;
+
+    // Getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public String getSemester() { return semester; }
+    public void setSemester(String semester) { this.semester = semester; }
+
+    public String getInterestedDomain() { return interestedDomain; }
+    public void setInterestedDomain(String interestedDomain) { this.interestedDomain = interestedDomain; }
+}
