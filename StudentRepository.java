a4202f3a853b56240e210c97a4b2aa50fdd94b76
blessy-JG123 <blessyjg105@gmail.com>
2025-10-28 15:35:20 +0530

    StudentRepository.java

--- StudentRepository.java
+++ StudentRepository.java
@@ -1,9 +1,7 @@
 package com.example.student.management.system.repository;
 
+import com.example.student.management.system.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.student.management.system.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
